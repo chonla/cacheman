d3bc9205a73296b89660f02ea1ef08ee8a6b3159
Chonlasith Jucksriporn <chonlasith@gmail.com>
2020-11-23 15:45:39 +0700

    Use BigCacheClient instead of bigcache package

---
+++ adapter_bigcache.go
@@ -0,0 +1,42 @@
+package cacheman
+
+import (
+	"time"
+
+	"github.com/allegro/bigcache"
+)
+
+type BigCacheClient struct {
+	client *bigcache.BigCache
+}
+
+// NewBigCache creates big cache client
+func NewBigCache(config *Config) (*BigCacheClient, error) {
+	ttl, e := time.ParseDuration(config.TTL)
+	if e != nil {
+		ttl, _ = time.ParseDuration(defaultTTL)
+	}
+	client, e := bigcache.NewBigCache(bigcache.DefaultConfig(ttl))
+	if e != nil {
+		return nil, e
+	}
+	return &BigCacheClient{
+		client: client,
+	}, nil
+}
+
+func (c *BigCacheClient) Get(key string) ([]byte, error) {
+	return c.client.Get(key)
+}
+
+func (c *BigCacheClient) Set(key string, value []byte) error {
+	return c.client.Set(key, value)
+}
+
+func (c *BigCacheClient) Delete(key string) error {
+	return c.client.Delete(key)
+}
+
+func (c *BigCacheClient) Reset() error {
+	return c.client.Reset()
+}
