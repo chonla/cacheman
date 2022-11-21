7b016eba199a72c63f925578c752a509dc40fba1
adisak srisuriyasavad <adisak.srisuriyasavad@oivan.com>
2022-11-21 11:10:29 +0700

    remove comment code

--- middleware.go
+++ middleware.go
@@ -9,60 +9,6 @@ import (
 	echo4 "github.com/labstack/echo/v4"
 )
 
-// Middleware creates a middleware to handle cache
-// func Middleware(config *Config, cache CacheInterface) echo.MiddlewareFunc {
-// 	cm = NewCacheManager(config, cache)
-// 	return func(next echo.HandlerFunc) echo.HandlerFunc {
-// 		return func(ctx echo.Context) error {
-// 			if cm.Enabled {
-// 				cm.Log(fmt.Sprintf("Test path: %s", ctx.Request().RequestURI))
-// 				if ctx.Request().Method == "GET" {
-// 					if enabledByPath(config.CacheInfoPath, ctx.Request().URL.Path) {
-// 						cm.Log("Cache info request")
-// 						cm.WriteInfo(ctx)
-// 					} else {
-// 						if cm.TestPath(ctx.Request().URL.Path) {
-// 							cm.Log(fmt.Sprintf("Path matches: %s", ctx.Request().RequestURI))
-
-// 							interceptor := NewInterceptor(ctx.Response().Writer)
-// 							ctx.Response().Writer = interceptor
-
-// 							if !cm.TryWrite(ctx) {
-// 								e := next(ctx)
-// 								// Store into cache only if status is 200
-// 								if e == nil && interceptor.Status() == 200 {
-// 									content := Content{
-// 										Status:  interceptor.Status(),
-// 										Headers: interceptor.Header(),
-// 										Content: base64.StdEncoding.EncodeToString(interceptor.Content()),
-// 									}
-// 									stringifiedCache, e := json.Marshal(content)
-// 									if e == nil {
-// 										cacheKey := ctx.Request().RequestURI
-// 										cm.Set(cacheKey, stringifiedCache)
-// 									}
-// 								}
-// 								return e
-// 							}
-// 							return nil
-// 						} else {
-// 							cm.Log(fmt.Sprintf("Path does not match: %s", ctx.Request().RequestURI))
-// 						}
-// 					}
-// 				} else {
-// 					if ctx.Request().Method == "PURGE" && enabledByPath(config.PurgePath, ctx.Request().URL.Path) {
-// 						cm.Purge()
-// 						ctx.NoContent(http.StatusOK)
-// 						return nil
-// 					}
-// 					cm.Log(fmt.Sprintf("Method does not match: %s", ctx.Request().Method))
-// 				}
-// 			}
-// 			return next(ctx)
-// 		}
-// 	}
-// }
-
 // MiddlewareV4 creates a middleware to handle cache for echo V4
 func MiddlewareV4(config *Config, cache CacheInterface) echo4.MiddlewareFunc {
 	cm = NewCacheManager(config, cache)
